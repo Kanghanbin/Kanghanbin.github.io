c074b6d2ee65939c0d95e0bc26195ee1cb8e9004
kanghanbin <1975386770@qq.com>
2019-09-16 17:50:22 +0800

    Site updated: 2019-09-16 17:49:57

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Tue Aug 20 2019 17:46:51 GMT+0800 (GMT+08:00)
+// build time:Mon Sep 16 2019 17:49:35 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var t=$(".sidebar-inner");var e=CONFIG.sidebar.offset?CONFIG.sidebar.offset:12;function i(){return $(".header-inner").height()+e}function r(){var t=$(".footer-inner");var e=t.outerHeight(true)-t.outerHeight();var i=t.outerHeight(true)+e;return i}function n(t){return $("#sidebar").css({"margin-top":t})}function a(){var a=i();var f=r();var o=$("#sidebar").height()+NexT.utils.getSidebarb2tHeight();var s=$("#content").height();if(a+o<s){t.affix({offset:{top:a-e,bottom:f}})}n(a).css({"margin-left":"initial"})}function f(){$(window).off(".affix");t.removeData("bs.affix").removeClass("affix affix-top affix-bottom");a()}function o(){var t=window.matchMedia("(min-width: 991px)");t.addListener(function(t){if(t.matches){f()}})}a();o()});
 //rebuild by neat 
