70275dc0fba0cc56bfc6cf26a53f737ca49d81ae
kanghanbin <1975386770@qq.com>
2019-12-02 17:27:38 +0800

    Site updated: 2019-12-02 17:27:04

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Thu Nov 28 2019 16:41:12 GMT+0800 (GMT+08:00)
+// build time:Mon Dec 02 2019 17:25:24 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]===o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
